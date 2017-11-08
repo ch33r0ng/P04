58b69b00f50e621778a80e31e02d6d2e5b17ab9f
161441Q TAN CHEE RONG <161441Q@sit.nyp.edu.sg>
2017-11-08 11:23:45 +0800

    gitgud

--- MathAdd.java
+++ MathAdd.java
@@ -3,6 +3,7 @@ public class MathAdd {
 	public MathAdd {}
 	
 	static public int add(int a, int b) {   
+	System.out.println(a+b);  
         return a + b; 
 	}
 }
---
+++ MathSub.java
@@ -0,0 +1,9 @@
+public class MathAdd {
+	
+	public MathAdd {}
+	
+	static public int add(int a, int b) { 
+	System.out.println(a-b);  
+        return a - b; 
+	}
+}
